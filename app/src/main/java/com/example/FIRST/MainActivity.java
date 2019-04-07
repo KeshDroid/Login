ba3145675c4dfd976049686a142517db503ecdf3
KeshDroid <ktrajapaksha27@gmail.com>
2019-04-07 15:17:37 +0530

    3.0

--- app/src/main/java/com/example/FIRST/MainActivity.java
+++ app/src/main/java/com/example/FIRST/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private Button Login;
     private TextView Info;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/FIRST/SecondActivity.java
+++ app/src/main/java/com/example/FIRST/SecondActivity.java
@@ -1,13 +1,33 @@
 package com.example.FIRST;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
+        private Button btnNext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenu();
+            }
+        });
+
+
     }
+        public void openMenu(){
+            Intent intent = new Intent(SecondActivity.this,Menu.class);
+            startActivity(intent);
+        }
 }
